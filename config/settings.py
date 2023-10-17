044e3ad567cd9ad5ffafbcad748f7ec33e5233aa
ekaterinarogova <ekatersergeev@gmail.com>
2023-10-17 20:43:59 +0300

    added CORS

--- config/settings.py
+++ config/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'corsheaders',
 
     'materials.apps.MaterialsConfig',
     'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -143,4 +145,13 @@ REST_FRAMEWORK = {
             'rest_framework.permissions.IsAuthenticated',
         ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "https://read-only.example.com",
+    "https://read-and-write.example.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://read-and-write.example.com",
+]
 
